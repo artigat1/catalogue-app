779520fe0ec093d2cdeda57cf8d780d1a157c79e
Steve Doggett <steve.doggett@mopowered.co.uk>
2016-03-28 16:26:03 +0100

    Override password defaults
    Don't want the password constraints imposed y default, so overriding
    them to what I do want.

--- src/catalogue-app/Startup.cs
+++ src/catalogue-app/Startup.cs
@@ -44,7 +44,15 @@ namespace catalogue_app
                 .AddDbContext<ApplicationDbContext>(options =>
                     options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+            services.AddIdentity<ApplicationUser, IdentityRole>(
+                o =>
+                {
+                    o.Password.RequireDigit = false;
+                    o.Password.RequireLowercase = false;
+                    o.Password.RequireNonLetterOrDigit = false;
+                    o.Password.RequireUppercase = false;
+                    o.Password.RequiredLength = 6;
+                })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
